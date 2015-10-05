8d3b433d3dd5f187858bbc382fcc8e604e13200a
William Setterberg <settwi@student.breckschool.org>
2015-10-04 23:12:39 -0500

    see previous commit

--- src/notelist.h
+++ src/notelist.h
@@ -21,7 +21,7 @@ typedef struct NoteList {
 
 NoteList *NoteList_add(NoteList *, uint8_t, size_t);
 NoteList *NoteList_find(NoteList *, uint8_t);
-NoteList *NoteList_firstOn(NoteList *);
+uint8_t NoteList_maxChannel(NoteList *);
 void NoteList_print(NoteList *, FILE *);
 void NoteList_destroy(NoteList **);
 
