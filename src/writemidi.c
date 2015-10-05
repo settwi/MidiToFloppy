d712bc37f07d66f642471548abd88bc14eb52366
William Setterberg <settwi@student.breckschool.org>
2015-10-04 23:10:09 -0500

    added crappily formatted arrays for each drive

--- src/writemidi.c
+++ src/writemidi.c
@@ -1,13 +1,13 @@
 #include "writemidi.h"
 
-// Maybe we should just print "C3" instead of stupid macros
 void writeMidi(MidiInfo *mi, const char *name)
 {
     if (!name)
         die(mi, "Supply a valid name");
     if (!mi->nl)
         die(mi, "No nl to print");
-    // NoteList_print(mi->nl, stdout);
+
+    uint8_t maxChannel = NoteList_maxChannel(mi->nl);
 
     const char *notes[] = {
         "C", "CS", "D", "DS", "E",
@@ -15,18 +15,34 @@ void writeMidi(MidiInfo *mi, const char *name)
         "AS", "B"
     };
 
-	fprintf(mi->out, "// Format is: note, length, channel\n\n");
-	fprintf(mi->out, "struct MidiNote %s[] = {", name);
-	size_t i = 0;
-	NoteList *nl = mi->nl;
-	if (!nl) die(mi, "Invalid notelist");
-	do {
-		if (i % 2 == 0) fprintf(mi->out, "\n\t");
+    fprintf(mi->out, "// Format is: note, length, channel\n\n");
+    
+    for (uint8_t drive = 0; drive < maxChannel + 1; ++drive) {
+        fprintf(mi->out,
+                "struct MidiNote %s%" PRIu8 "[] = {",
+                name, drive);
 
-		fprintf(mi->out, "{ %s%d, %zu, %" PRIu8 " }, ",
-			    notes[nl->note % 12], nl->note / 12 - 1,
-                nl->length * mi->tickTime, nl->channel);
-	} while(++i, (nl = nl->next));
-	fprintf(mi->out, "\n\t{ DONE, (size_t)-1, (uint8_t)-1 }");
-	fprintf(mi->out, "\n}\n");
+        size_t i = 0;
+        NoteList *nl = mi->nl;
+
+        do {
+            if (i % 2 == 0) fprintf(mi->out, "\n\t");
+            
+            if (nl->channel == drive)
+                fprintf(mi->out, "{ %s%" PRIu8 ", %zu, %" PRIu8 " }, ",
+                        notes[nl->note % 12],
+                        (uint8_t)(nl->note / 12 - 1),   // Suppress those warnings!
+                        nl->length * mi->tickTime, nl->channel);
+            
+            else if (drive != 0)
+                fprintf(mi->out, "{ REST, %zu, REST }, ",
+                        nl->length * mi->tickTime);
+            else
+                --i;
+
+        } while(++i, (nl = nl->next));
+        
+        fprintf(mi->out, "\n\t{ DONE, (size_t)-1, (uint8_t)-1 }");
+        fprintf(mi->out, "\n}\n");
+    }
 }
